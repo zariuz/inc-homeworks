a036d9dd91e2ac5f7f59f53abe4eff7e8e008658
zariuz <nechetov@hotmail.com>
2021-08-28 16:53:14 +0300

    hw9 completed

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,52 +1,58 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
-    const [show, setShow] = useState<boolean>(false)
+  const [timerId, setTimerId] = useState<number>(0)
+  const [date, setDate] = useState<Date>()
+  const [show, setShow] = useState<boolean>(false)
 
-    const stop = () => {
-        // stop
-    }
-    const start = () => {
-        stop()
-        const id: number = window.setInterval(() => {
-            // setDate
-        }, 1000)
-        setTimerId(id)
-    }
+  const stop = () => {
+    clearInterval(timerId)
+  }
+  const start = () => {
+    stop()
+    const id: number = window.setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    setTimerId(id)
+  }
 
-    const onMouseEnter = () => {
-        // show
-    }
-    const onMouseLeave = () => {
-        // close
-    }
+  const onMouseEnter = () => {
+    setShow(true)
+  }
+  const onMouseLeave = () => {
+    setShow(false)
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+  }
 
-    return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
+  const stringTime = date?.toLocaleTimeString()
+  const stringDate = date?.toLocaleDateString()
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+  return (
+    <div className={s.wrapper}>
+      <div className={s.time}>
+        <div
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        >
+          {stringTime}
         </div>
-    )
+
+        {show && (
+          <div>
+            {stringDate}
+          </div>
+        )}
+      </div>
+
+      <div >
+        <SuperButton onClick={start} className={s.button}>start</SuperButton>
+        <SuperButton onClick={stop} className={s.button}>stop</SuperButton>
+      </div>
+
+    </div>
+  )
 }
 
 export default Clock
